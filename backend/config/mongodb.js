fc62740790a2378e114164e00f5a297078045491
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-15 02:17:35 +0600

    first commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//LOCAL_DB_CONNECT  -localhost database
+//DB_CONNECT  -online database
+dotenv.config();
+const connection_url = process.env.LOCAL_DB_CONNECT;
+
+mongoose.connect(
+  'mongodb+srv://jeweldb:Nq3euqdpmTTNAp8d@cluster0.ukmc1.mongodb.net/erp-education',
+  // 'mongodb+srv://sms:RVy6PD1V2ljiOCJS@school-management-syste.zeq56.mongodb.net/school-management-sys?retryWrites=true&w=majority',
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
+
+mongoose.connection.once('open', () => {
+  console.log('db connnected localhost db');
+  // gfs = new mongoose.mongo.GridFSBucket(connect.db, {
+  //     bucketName: "uploads"
+  // })
+});
+
+//export default mongoose;
+module.exports = mongoose;
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+const path = require('path');
+//const __dirname = path.resolve(path.dirname(""));
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/consumerPhotos'));
+app.use(express.static('./public'));
+
+//routes
+app.get('/', (req, res) => {
+  res.send('welcome to D-system api');
+});
+
+
+
+app.listen(PORT, () => {
+  return console.log(`listening on port ${PORT}`);
+});
