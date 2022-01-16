708c302d79c46dc038ca7ce2272308a758b85090
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-16 23:35:33 +0600

    correspondance model added

---
+++ backend/models/CorrespondanceModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const CorrespondanceSchema = new Schema({
+    address: {
+        type: String,
+        required: true,
+    },
+    salutation: {
+        type: String,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+    },
+    closing: {
+        type: String,
+    },
+    signature: {},
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("correspondance", CorrespondanceSchema);
