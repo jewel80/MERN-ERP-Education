d87f17bfd11ecfff5b15a60d272f87e08d1798b2
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-16 20:05:24 +0600

    Deduction Router Added.

--- backend/routes/DeductionsRoutes.js
+++ backend/routes/DeductionsRoutes.js
@@ -3,7 +3,13 @@ const DeductionsModel = require("../models/DeductionsModel");
 
 const route = express.Router();
 
-
+//get all events
+route.get("/", async(req, res) => {
+    const data =await DeductionsModel.find().sort({
+        createAt:"desc"
+    })
+    res.json(data)
+})
 
 //create
 route.post("/create", async (req, res) => {
@@ -23,5 +29,44 @@ route.post("/create", async (req, res) => {
             });
         });
 });
+
+//edit
+route.put("/update/:id", (req, res) => {
+    if (!req.params.id) {
+      return res.status(400).send("Missing URL parameter: Deduction Name");
+    }
+    DeductionsModel.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      req.body,
+      {
+        new: true,
+      }
+    ).then((doc) => {
+        if (!doc) {
+          return res.json({ success: false, error: "doex not exists" });
+        }
+        return res.json({ success: true, doc });
+      })
+      .catch((err) => {
+        res.json({ success: false, message: err });
+      });
+  });
+  
+  route.delete("/delete/:id", (req, res) => {
+    if (!req.params.id) {
+      return res.status(400).send("Missing URL parameter: username");
+    }
+    DeductionsModel.findOneAndRemove({
+      _id: req.params.id,
+    })
+      .then((doc) => {
+        res.json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = route;
