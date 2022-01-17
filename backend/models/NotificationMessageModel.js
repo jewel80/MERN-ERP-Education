33b79be2f1e94f37eaa74bdb016feac1091a9e07
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-17 23:14:02 +0600

    NewMessage Model added

---
+++ backend/models/NotificationMessageModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const NotificationsSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    sender: {
+        type: String,
+    },
+    isSend: {
+        type: Boolean,
+        default: false,
+    },
+    messageID: {
+        type: String,
+    },
+    receiver: {
+        type: String,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("newMessage", NotificationsSchema);
