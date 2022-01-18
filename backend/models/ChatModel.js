ea01070b32dccdce2a38bfa630d9f2fe291ab1b0
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-18 20:51:01 +0600

    Chat model  added

---
+++ backend/models/ChatModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const ChatSchema = new Schema({
+    userID: {
+        type: String,
+    },
+    message: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    parent: {
+        type: String,
+    },
+    sender: {
+        type: String,
+    },
+    requestor_id: {
+        type: String,
+    },
+    acceptor_id: {
+        type: String,
+    },
+    messages: {
+        type: [{
+            senderID: String,
+            message: String,
+            role: String,
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        }, ],
+        default: [],
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("chats", ChatSchema);
