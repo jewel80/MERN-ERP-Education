11158e51a36ad0deba7cb3b2892311466b70e1de
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-19 19:57:09 +0600

    Scholarships model added

---
+++ backend/models/ScholarshipsModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const ScholarshipsSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    percentage: {
+        type: String,
+    },
+    types: {
+        type: Object,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("scholarships", ScholarshipsSchema);
