b5b04d4ea2a0cf2a8f990196781fd9ae6acb74da
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-19 19:51:21 +0600

    SBA model added

---
+++ backend/models/SBAModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const SBASchema = new Schema({
+    class: {
+        type: String,
+            required: true,
+    },
+    course: {
+        type: String,
+    },
+    academicYear: {
+        type: String,
+    },
+    term: {
+        type: String,
+    },
+    exam: {
+        type: Number,
+    },
+    classWork: {
+        type: Number,
+    },
+    students: {
+        type: [{
+            userID: String,
+            name: String,
+            position: String,
+            exam: Number,
+            classWork: Number,
+        }, ],
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("sba", SBASchema);
