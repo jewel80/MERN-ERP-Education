9836a5cf5c9455c1e5e75be0a8de55fe3cf25609
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-19 20:13:52 +0600

    Model added

---
+++ backend/models/YearGroupModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("../config/mongodb");
+
+const {
+    Schema
+} = mongoose;
+
+const YearGroupSchema = new Schema({
+    name: {
+        type: String,
+    },
+    code: {
+        type: String,
+    },
+    year: {
+        type: String,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("yeargroup", YearGroupSchema);
